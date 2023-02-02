eff67e79e0d51fd18f83365894c678ff66d72403
shahid iqbal <109196236+shahidiqbalofficial@users.noreply.github.com>
2023-02-02 11:00:42 +0500

    Add files via upload

---
+++ Login App/index.js
@@ -0,0 +1,12 @@
+// /**
+//  * @format
+//  */
+
+//  import {AppRegistry} from 'react-native';
+//  import App from './App';
+//  import {name as appName} from './app.json';
+// import LoginScreen from './src/Components/views/screens/LoginScreen';
+ 
+//  AppRegistry.registerComponent(appName, () => App);
+
+//  export default LoginScreen
